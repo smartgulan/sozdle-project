551a60ca19a7019927424fa7ca5f4ae0aad26ffa
Margulan <m.begzhan@aster.kz>
2025-01-10 14:50:26 +0500

    word validation added

--- back-end/main.go
+++ back-end/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"unicode"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -10,9 +12,24 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		word := c.Query("word")
-
+		if wordValidation(word) == false {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid word"})
+			return
+		}
 		c.JSON(http.StatusOK, gin.H{"word": word})
 	})
 
 	r.Run(":8080")
+}
+
+func wordValidation(word string) bool {
+	if len(word) != 5 {
+		return false
+	}
+	for _, char := range word {
+		if unicode.IsDigit(char) {
+			return false
+		}
+	}
+	return true
 }
